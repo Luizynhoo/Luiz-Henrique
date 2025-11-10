f65cbda44ba5a02531e7375d95821ca0f00984b5
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2025-11-10 09:16:04 -0300

    Add files via upload

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef } from 'react';
+import { useHorizontalScroll } from './hooks/useHorizontalScroll';
+import Navbar from './components/layout/Navbar';
+import HomeSection from './components/sections/HomeSection';
+// import AboutSection from './components/sections/AboutSection';
+// import SkillsSection from './components/sections/SkillsSection';
+// import ProjectsSection from './components/sections/ProjectsSection';
+// import ContactSection from './components/sections/ContactSection';
+import './styles/global.css';
+
+function App() {
+  const scrollRef = useRef(null);
+  useHorizontalScroll(scrollRef);
+
+  return (
+    <div className="app-container">
+      <Navbar /> 
+      
+      <div ref={scrollRef} className="horizontal-container">
+        <HomeSection />
+        {/* <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection /> */}
+      </div>
+    </div>
+  );  
+}
+
+export default App;
